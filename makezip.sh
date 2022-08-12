7cfb4a8afc5a72a75127918fdc8271bd37a2efd0
Graeme Cole <graeme@greem.co.uk>
2022-08-12 21:39:35 +0100

    Improve makezip.sh - include top-level directory in zip file
    makezip.sh now makes a zip file containing a directory, which in turn
    contains all of Atropine's files. This is slightly preferable to the zip
    file containing all of Atropine's files directly, because the temptation
    for the user is to say "extract to my desktop, please!" then find they
    have all of Atropine's innards smeared over their desktop rather than
    being in all one folder.
    
    makezip.sh now takes a single, mandatory argument: the version number
    we're building. If the version number is x.y.z, the zip file and its
    top-level directory will be named atropine-x.y.z.

--- makezip.sh
+++ makezip.sh
@@ -1,6 +1,81 @@
 #!/bin/bash
 
-rm atropine.zip 2>/dev/null
-zip -r atropine.zip generators py licence.txt tourneys atropine.py fixpaths.sh webroot -x \*.db \*.pyc
+# makezip.sh
+# Package up Atropine into a zip file suitable for distribution.
 
-exit $?
+ATROPINE_VERSION=$1
+if [ -z "$ATROPINE_VERSION" ]; then
+    # Display help
+    echo "Usage: makezip.sh <version>"
+    echo "<version> is in the form x.y.z[-extrastuff]"
+    exit 1
+fi
+
+# Validate version number format.
+echo "$ATROPINE_VERSION" | egrep "^[0-9]+\.[0-9]+\.[0-9]+" > /dev/null
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+    echo "Version number must be of the form x.y.z where x, y and z are integers."
+    echo "It may optionally be followed by additional cruft."
+    exit 1
+fi
+
+# The zip file will contain a directory with this version in its name, so make
+# sure it doesn't contain any characters problematic for Windows.
+echo "$ATROPINE_VERSION" | egrep "[/<>\\\\:\"|?*[:cntrl:]]" >/dev/null
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+    echo "Version number may not contain any characters which would be illegal in a"
+    echo "Windows filename. These include the control characters and the following:"
+    echo "\\ / < > : \" ? | *"
+    exit 1
+fi
+
+# Windows filenames may not end with a space or a dot.
+echo "$ATROPINE_VERSION" | egrep '\.$| $' >/dev/null
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+    echo "Version number may not end with a space or a dot."
+    exit 1
+fi
+
+# START_DIR: our current working directory, which contains all the Atropine
+# source files.
+START_DIR=$(pwd)
+
+# TEMP_DIR: a temporary directory in which we'll package up the zip
+TEMP_DIR=$(mktemp -d --tmpdir)
+
+# VER_DIR: the top-level directory of the zip file, which contains all the
+# files. We have this directory so that when someone says "extract to my
+# desktop" they don't get all of Atropine's innards smeared over their desktop,
+# they get all of Atropine's innards neatly packaged inside a new folder.
+VER_DIR="atropine-$ATROPINE_VERSION"
+mkdir "$TEMP_DIR/$VER_DIR" || exit 1
+
+# Copy all the files we need to $TEMP_DIR/$VER_DIR, and change to $TEMP_DIR
+cp -a generators py webroot licence.txt atropine.py fixpaths.sh "$TEMP_DIR/$VER_DIR/" || exit 1
+
+cd "$TEMP_DIR" || exit 1
+
+ZIP_FILE_NAME="atropine-$ATROPINE_VERSION.zip"
+
+# Create an empty tourneys directory, the user will need that.
+mkdir "$VER_DIR/tourneys"
+
+# Zip up the contents of $VER_DIR into our new zip file, excluding cruft
+# we don't need.
+zip -r "$ZIP_FILE_NAME" "$VER_DIR" -x \*.db \*.pyc \*.swp || exit 1
+
+# We now have our zip file, but it's in $TEMP_DIR and we're about to delete
+# that, so put the zip file in the directory we started in.
+cp "$ZIP_FILE_NAME" "$START_DIR/" || exit 1
+
+# Report success and go back to our starting directory
+echo "Created $START_DIR/$ZIP_FILE_NAME"
+cd "$START_DIR"
+
+# Clean up the temporary directory and everything in it.
+rm -rf "$TEMP_DIR"
+
+exit 0
