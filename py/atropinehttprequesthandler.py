a178efbe9cc2629c02181a518d6621726c97b0ea
Graeme Cole <graeme@greem.co.uk>
2024-11-20 23:53:43 +0000

    Put body of big if/elif/elif/... statement in new functions
    Tidy up the big if... statement in atropinehttprequesthandler.py.

--- py/atropinehttprequesthandler.py
+++ py/atropinehttprequesthandler.py
@@ -4,6 +4,7 @@ import os
 import http.server
 import socketserver
 from urllib.parse import unquote
+from html import escape
 import httpresponse
 import countdowntourney
 import handlerutils
@@ -38,8 +39,9 @@ class AtropineHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     All HTTP requests are handled by AtropineHTTPRequestHandler.
     Requests to /service/<tourneyname>/<handlername> are passed to the
     appropriate handler module in ATROPINE_HANDLER_MODULES.
-    Requests to /cgi-bin/<script>.py or /atropine/<tourney>/<script> are passed
-    to the handler module in py/dynamicpages/.
+    Requests to /atropine/<tourney>/<script>
+    (formerly /cgi-bin/<script>?tourney=<tourney>) are passed to the handler
+    module in py/dynamicpages/.
 
     All other requests are handled by the SimpleHTTPRequestHandler, which
     returns the contents of a file.
@@ -50,6 +52,112 @@ class AtropineHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
 
     def add_webpage_module(handler_name, module):
         AtropineHTTPRequestHandler.ATROPINE_WEBPAGE_MODULES[handler_name] = module
+
+    def handle_redirect(self, location):
+        response = httpresponse.HTTPResponse()
+        response.set_status_code(301)
+        response.add_header("Location", location)
+        response.writeln("<html><head><title>301 Moved Permanently</title></head><body><p>This page doesn't exist any more. Redirecting to <a href=\"%(url)s\">%(url)s</a>...</p></body></html>" % {
+            "url" : escape(location)
+        })
+        handlerutils.send_response(self, response)
+
+    def handle_service_request(self, request_method, path_components, query_string):
+        # If we have a /service handler for this path, call it.
+        if len(path_components) < 3:
+            handlerutils.send_error_response(self, "Bad URL: format is /service/<tourneyname>/<servicename>/...", status_code=400)
+            return
+        tourney_name = path_components[1]
+        tourneys_path = os.getenv("TOURNEYSPATH")
+        handler_name = path_components[2]
+        remaining_path_components = path_components[3:]
+        if handler_name not in AtropineHTTPRequestHandler.ATROPINE_HANDLER_MODULES:
+            # Unknown handler name
+            handlerutils.send_error_response(self, "Bad URL: unknown service \"%s\"" % (handler_name), status_code=404)
+            return
+
+        try:
+            # Open this tourney DB file.
+            with countdowntourney.tourney_open(tourney_name, tourneys_path) as tourney:
+                # If this tourney exists and we opened it, call the
+                # handler's handle() method which will send the response to
+                # the client.
+                try:
+                    AtropineHTTPRequestHandler.ATROPINE_HANDLER_MODULES[handler_name].handle(self, tourney, remaining_path_components, query_string)
+                except Exception as e:
+                    handlerutils.send_error_response(self, "Failed to execute request handler %s for tourney %s: %s" % (handler_name, tourney_name, str(e)), status_code=400)
+        except countdowntourney.DBNameDoesNotExistException as e:
+            handlerutils.send_error_response(self, e.description, status_code=404)
+        except countdowntourney.TourneyException as e:
+            handlerutils.send_error_response(self, "Failed to open tourney \"%s\": %s" % (tourney_name, e.description), status_code=400)
+        except Exception as e:
+            handlerutils.send_error_response(self, "Failed to open tourney \"%s\": %s" % (tourney_name, str(e)), status_code=400)
+
+    def handle_page_request(self, request_method, path_components, query_string):
+        tourney_name = path_components[1]
+        handler_name = path_components[2]
+        extra_components = path_components[3:]
+        tourneys_path = os.getenv("TOURNEYSPATH")
+        if handler_name not in AtropineHTTPRequestHandler.ATROPINE_WEBPAGE_MODULES:
+            handlerutils.send_html_error_response(self, "%s: not found" % (self.path), status_code=404)
+            return
+
+        if handler_name not in WEBPAGE_HANDLERS_PUBLIC and not self.is_client_from_localhost():
+            # Non-localhost connection to a non-public page
+            handlerutils.send_html_error_response(self, "This page is only accessible from the same computer that is running Atropine.", status_code=403)
+            return
+
+        if request_method == "POST":
+            # Read this request's POST data
+            try:
+                content_length = int(self.headers.get("content-length"))
+            except ValueError:
+                handlerutils.send_html_error_response(self, "Bad request: request method is POST but no Content-Length header")
+                return
+            post_data = self.rfile.read(content_length)
+        else:
+            # No POST data for this request
+            post_data = None
+
+        # Create an HTTPResponse for the script to put its response into...
+        response = httpresponse.HTTPResponse()
+
+        # Package up the name=value settings from the query string and any
+        # POST data into a FieldStorage object.
+        field_storage = fieldstorage.FieldStorage(
+                content_type=self.headers.get("content-type"),
+                request_method=request_method, query_string=query_string,
+                post_data=post_data)
+
+        # Most former CGI scripts had a tourney=... parameter - validate
+        # that here and open the countdowntourney object, to avoid every
+        # individual script having to do it.
+        try:
+            if webpage_handler_needs_tourney(handler_name):
+                if not tourney_name:
+                    handlerutils.send_html_error_response(self, "No tourney name specified.", status_code=400)
+                    return
+
+            handler_module = AtropineHTTPRequestHandler.ATROPINE_WEBPAGE_MODULES[handler_name]
+            if not webpage_handler_needs_tourney(handler_name):
+                handler_module.handle(self, response, None, request_method, field_storage, query_string, extra_components)
+                handlerutils.send_response(self, response)
+            else:
+                # Open this tourney DB file.
+                with countdowntourney.tourney_open(tourney_name, tourneys_path) as tourney:
+                    # If this tourney exists and we opened it, call the
+                    # handler's handle() method which will send the
+                    # response to the client.
+                    handler_module.handle(self, response, tourney, request_method, field_storage, query_string, extra_components)
+                    handlerutils.send_response(self, response)
+        except countdowntourney.DBNameDoesNotExistException as e:
+            handlerutils.send_html_error_response(self, e.description, status_code=404)
+        except countdowntourney.TourneyException as e:
+            # If TourneyException is thrown by a page module, it's a bug
+            handlerutils.send_exception_response(self, e, status_code=500)
+        except Exception as e:
+            # If any other exception is thrown by a page module, it's a bug
+            handlerutils.send_exception_response(self, e, status_code=500)
 
     def atropine_handle_request(self, request_method):
         # Split up "path" into its path component and query string
@@ -75,117 +183,25 @@ class AtropineHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
             # "helpfully" autocomplete http://localhost:3960 to
             # http://localhost:3960/cgi-bin/home.py, so redirect to the
             # correct location.
-            response = httpresponse.HTTPResponse()
-            response.set_status_code(301)
-            response.add_header("Location", "/")
-            response.writeln("<html><head><title>301 Moved Permanently</title></head><body><p>This page doesn't exist any more. Redirecting to <a href=\"/\">/ (the root)</a></p></body></html>")
-            handlerutils.send_response(self, response)
-            return True
+            self.handle_redirect("/")
         elif len(path_components) >= 1 and path_components[0] == "service":
-            # If we have a /service handler for this path, call it.
-            if len(path_components) < 3:
-                handlerutils.send_error_response(self, "Bad URL: format is /service/<tourneyname>/<servicename>/...", status_code=400)
-                return True
-            tourney_name = path_components[1]
-            handler_name = path_components[2]
-            remaining_path_components = path_components[3:]
-            if handler_name not in AtropineHTTPRequestHandler.ATROPINE_HANDLER_MODULES:
-                # Unknown handler name
-                handlerutils.send_error_response(self, "Bad URL: unknown service \"%s\"" % (handler_name), status_code=404)
-                return True
-
-            try:
-                # Open this tourney DB file.
-                with countdowntourney.tourney_open(tourney_name, tourneys_path) as tourney:
-                    # If this tourney exists and we opened it, call the
-                    # handler's handle() method which will send the response to
-                    # the client.
-                    try:
-                        AtropineHTTPRequestHandler.ATROPINE_HANDLER_MODULES[handler_name].handle(self, tourney, remaining_path_components, query_string)
-                    except Exception as e:
-                        handlerutils.send_error_response(self, "Failed to execute request handler %s for tourney %s: %s" % (handler_name, tourney_name, str(e)), status_code=400)
-            except countdowntourney.DBNameDoesNotExistException as e:
-                handlerutils.send_error_response(self, e.description, status_code=404)
-            except countdowntourney.TourneyException as e:
-                handlerutils.send_error_response(self, "Failed to open tourney \"%s\": %s" % (tourney_name, e.description), status_code=400)
-            except Exception as e:
-                handlerutils.send_error_response(self, "Failed to open tourney \"%s\": %s" % (tourney_name, str(e)), status_code=400)
-            return True
+            # /service/<tourneyname>/<handlername>/...
+            # A URL for internal use that probably returns some JSON.
+            # Pass the request to the appropriate module in py/httphandler/.
+            self.handle_service_request(request_method, path_components, query_string)
         elif len(path_components) >= 3 and path_components[0] == "atropine":
             # /atropine/<tourneyname>/<modulename>/...
             # It's an old link to what used to be a CGI script.
-            # Pass this request to the appropriate module in py/dynamicpages.
-            tourney_name = path_components[1]
-            handler_name = path_components[2]
-            extra_components = path_components[3:]
-            if handler_name not in AtropineHTTPRequestHandler.ATROPINE_WEBPAGE_MODULES:
-                handlerutils.send_html_error_response(self, "Bad URL: %s not found" % (self.path), status_code=404)
-                return True
-
-            if handler_name not in WEBPAGE_HANDLERS_PUBLIC and not self.is_client_from_localhost():
-                # Non-localhost connection to a non-public page
-                handlerutils.send_html_error_response(self, "This page is only accessible from the same computer that is running Atropine.", status_code=403)
-                return True
-
-            if request_method == "POST":
-                # Read this request's POST data
-                try:
-                    content_length = int(self.headers.get("content-length"))
-                except ValueError:
-                    handlerutils.send_html_error_response(self, "Bad request: request method is POST but no Content-Length header")
-                    return True
-                post_data = self.rfile.read(content_length)
-            else:
-                # No POST data for this request
-                post_data = None
-
-            # Create an HTTPResponse for the script to put its response into...
-            response = httpresponse.HTTPResponse()
-
-            # Package up the name=value settings from the query string and any
-            # POST data into a FieldStorage object.
-            field_storage = fieldstorage.FieldStorage(
-                    content_type=self.headers.get("content-type"),
-                    request_method=request_method, query_string=query_string,
-                    post_data=post_data)
-
-            # Most former CGI scripts had a tourney=... parameter - validate
-            # that here and open the countdowntourney object, to avoid every
-            # individual script having to do it.
-            try:
-                if webpage_handler_needs_tourney(handler_name):
-                    if not tourney_name:
-                        handlerutils.send_html_error_response(self, "No tourney name specified.", status_code=400)
-                        return True
-
-                handler_module = AtropineHTTPRequestHandler.ATROPINE_WEBPAGE_MODULES[handler_name]
-                if not webpage_handler_needs_tourney(handler_name):
-                    handler_module.handle(self, response, None, request_method, field_storage, query_string, extra_components)
-                    handlerutils.send_response(self, response)
-                else:
-                    # Open this tourney DB file.
-                    with countdowntourney.tourney_open(tourney_name, tourneys_path) as tourney:
-                        # If this tourney exists and we opened it, call the
-                        # handler's handle() method which will send the
-                        # response to the client.
-                        handler_module.handle(self, response, tourney, request_method, field_storage, query_string, extra_components)
-                        handlerutils.send_response(self, response)
-            except countdowntourney.DBNameDoesNotExistException as e:
-                handlerutils.send_html_error_response(self, e.description, status_code=404)
-            except countdowntourney.TourneyException as e:
-                # If TourneyException is thrown by a page module, it's a bug
-                handlerutils.send_exception_response(self, e, status_code=500)
-            except Exception as e:
-                # If any other exception is thrown by a page module, it's a bug
-                handlerutils.send_exception_response(self, e, status_code=500)
-
-            # Return true to say we handled this
-            return True
+            # Pass this request to the appropriate module in py/dynamicpages/.
+            self.handle_page_request(request_method, path_components, query_string)
         else:
             # Return false to say this needs passing to the appropriate
             # method in SimpleHTTPRequestHandler - it's just a plain fetch of a
             # file from a directory.
             return False
+
+        # Return true to say we handled this request.
+        return True
 
     def do_GET(self):
         if not self.atropine_handle_request("GET"):
